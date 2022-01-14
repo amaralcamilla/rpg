bc643a0abd4da75b08131518c2c97e2ffa043419
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-13 23:08:00 -0300

    Adiciona armor, potion e attack.

--- AppRpg.java
+++ AppRpg.java
@@ -6,7 +6,7 @@ public class AppRpg {
 	
 	private static Scanner keyboard;
 	private static String playerName;
-	private static int level, sex, combatClass, weapon, motivation, moveOn, crossing;
+	private static int level, sex, combatClass, weapon, motivation, moveOn, crossing, armor, potion, attack;
 	
 	public static void main (String [] args) {
 		keyboard = new Scanner(System.in);
@@ -24,7 +24,7 @@ public class AppRpg {
 		System.out.println("Digite seu nome de combate: ");
 		playerName = keyboard.next();
 		
-		System.out.println("Boa escolha, senti firmeza!");
+		System.out.println("Boa escolha " + playerName + ", senti firmeza!");
 		System.out.println("Selecione o sexo: ");
 		System.out.println("1: Masculino");
 		System.out.println("2: Feminino");
@@ -106,9 +106,10 @@ public class AppRpg {
 		
 		if (moveOn == 1) {
 			System.out.println("Você caminha, atento a todos os seus sentidos, por vários metros, até visualizar a frente uma fonte de luz, que você imagina ser a chama de uma tocha, vindo de dentro de uma porta aberta.");
-			
 		} else if (moveOn == 2) {
 			System.out.println("O medo invade o seu coração e você sente que ainda não está à altura do desafio. Você se volta para a noite lá fora e corre em direção à segurança.");
+			
+			//TODO sai do jogo.
 			
 		} else {
 			System.out.println("Digite uma opção válida.");
@@ -119,12 +120,14 @@ public class AppRpg {
 		System.out.println("2: Correndo");
 		System.out.println("3: Saltando");
 		
+		crossing = keyboard.nextInt();
 		
 		switch (crossing) {
 		case 1: {
 			System.out.println("Você toma cuidado e vai caminhando vagarosamente em direção à luz. Quando você pisa exatamente embaixo da porta, você sente o chão ceder levemente, como se tivesse pisado em uma pedra solta. Você ouve um ruído de mecanismos se movimentando, e uma escotilha se abre no teto atrás de você, no corredor. Flechas voam da escotilha em sua direção, e você salta para dentro da sala, porém uma delas te acerta na perna.");
 			
 			// TODO adiciona dano da flecha.
+			
 			break;
 				}
 		case 2: {
@@ -139,7 +142,80 @@ public class AppRpg {
 			throw new Error ("Digite uma opção válida.");
 				} 
 		
-		crossing = keyboard.nextInt();
+		// TODO texto: System.out.println("Você se encontra sozinho...")
+		// TODO loop de combate 1 - porta direita
+		
+		
+		System.out.println("Após derrotar o Armeiro, você percebe que seus equipamentos estão muito danificados, e olha em volta, encarando todas aquelas peças de armaduras resistentes e em ótimo estado.");
+		System.out.println("1: Pegar uma nova armadura!");
+		System.out.println("2: Seguir com a armadura esculhambada.");
+		
+		armor = keyboard.nextInt();
+		
+		if (armor == 1) {
+			System.out.println("Você resolve usar os equipamentos do inimigo contra ele, e trocar algumas peças suas, que estavam danificadas, pelas peças de armaduras existentes na sala. De armadura nova, você se sente mais protegido para os desafios à sua frente.");
+			
+			//TODO acrescenta 5 pts de defesa.
+			
+		} else if (armor == 2) {
+			System.out.println("Você decide que não precisa utilizar nada que venha das mãos do inimigo.");
+			
+		} else {
+			System.out.println("Digite uma opção válida.");
+		}
+		
+		
+		// TODO texto: System.out.println("Em uma mesa, você encontra....")
+		// TODO loop de combate 2 - porta esquerda
+		
+		System.out.println("Após derrotar o Alquimista, você olha em volta, tentando reconhecer alguma poção do estoque do inimigo. Em uma mesa, você reconhece uma pequena garrafa de vidro contendo um líquido levemente rosado, pega a garrafa e pondera se deve beber um gole.");
+		System.out.println("1: Mal não vai fazer. Bora beber!");
+		System.out.println("2: Melhor não arriscar.");
+		
+		potion = keyboard.nextInt();
+		
+		if (potion == 1) {
+			System.out.println("Você se sente revigorado para seguir adiante!");
+			
+			//TODO recupera 100% dos pontos de vida.
+			
+		} else if (potion == 2) {
+			System.out.println("Você fica receoso de beber algo produzido pelo inimigo.");
+			
+		} else {
+			System.out.println("Digite uma opção válida.");
+		}
+		
+		// TODO texto: System.out.println("Ao lado da porta, você vê....")
+		
+		System.out.println("Ele percebe sua chegada e se levanta com um salto, apanhando seu machado de guerra de lâmina dupla.");
+		System.out.println("1: Atacar");
+		System.out.println("2: Esperar");
+		
+		attack = keyboard.nextInt();
+		
+		if (attack == 1) {
+						
+			//TODO loop de combate
+			
+		} 
+		while (attack == 2) {
+			System.out.println("Vai ficar aí parado?");
+			System.out.println("1: Atacar");
+			System.out.println("2: Esperar");
+		}
+			
+		
+		System.out.println("Você conseguiu!");
+		if (motivation == 1) {
+			System.out.println("Você obteve sua vingança. Você se ajoelha e cai no choro, invadido por uma sensação de alívio e felicidade. Você se vingou, tudo que sempre quis, está feito. Agora  você pode seguir sua vida.");
+		} else if (motivation == 2) {
+			System.out.println("O êxtase em que você se encontra não cabe dentro de si. Você se ajoelha e grita de alegria. A glória o aguarda, você a conquistou.");
+		} else {
+			System.out.println("Digite uma opção válida.");
+		}
+		
+		System.out.println("Você se levanta, olha para os prisioneiros, vai de um em um e os liberta, e todos vocês saem em direção à noite, retornando à cidade. Seu dever está cumprido.");
 		
 	}
 
