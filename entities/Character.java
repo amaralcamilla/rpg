adb214fb471d04d4e947fcff82a6524bfbedc9a6
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-13 23:13:53 -0300

    Create Character.java

---
+++ entities/Character.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+//Nome
+//Sexo
+//Classe de combate
+//Arma disponível
