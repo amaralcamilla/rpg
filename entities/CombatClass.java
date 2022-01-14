0296a27b0d8099e48ca5488becd73d1bcdb7a980
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-13 23:16:27 -0300

    Create CombatClass.java

---
+++ entities/CombatClass.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class CombatClass {
+
+}
+
+// Guerreiro/Guerreira: espada, machado, martelo
+// Caçador/Caçadora: arco+flecha, besta+virote
+// Sacerdote/Sacerdotisa: clava, livro
+// Mago/Feiticeira: cajado e livro
