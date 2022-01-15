75d58ee6fb0d741def00374db80cdc1303b35461
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-15 16:14:06 -0300

    Adiciona atributos e construtor.

--- entities/CombatClass.java
+++ entities/CombatClass.java
@@ -1,10 +1,36 @@
 package entities;
 
-public class CombatClass {
+public abstract class CombatClass {
+	private String combatClass;
+	private int attack;
+	private int defense;
+	private Weapon weapons;
+	
+	public CombatClass(String combatClass, int attack, int defense) {
+		super();
+		this.combatClass = combatClass;
+		this.attack = attack;
+		this.defense = defense;
+		this.weapons = weapons;
+	}
 
+	public String getCombatClass() {
+		return combatClass;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+	
+	
+	
 }
 
 // Guerreiro/Guerreira: espada, machado, martelo
 // Caçador/Caçadora: arco+flecha, besta+virote
 // Sacerdote/Sacerdotisa: clava, livro
-// Mago/Feiticeira: cajado e livro
+// Feiticeiro/Feiticeira: cajado e livro
