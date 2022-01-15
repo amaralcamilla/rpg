e2697054ed4686e72bbe4f7593e8db8800b225ee
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-15 16:14:50 -0300

    Adiciona atributos e construtor.

--- entities/Character.java
+++ entities/Character.java
@@ -1,20 +1,46 @@
 package entities;
 
 public class Character {
+	private String playerName;
+	private int life, attack, defense, sex, combatClass, weapon;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Character(String playerName, int life, int attack, int defense, int sex, int combatClass, int weapon) {
+		super();
+		this.playerName = playerName;
+		this.life = life;
+		this.attack = attack;
+		this.defense = defense;
+		this.sex = sex;
+		this.combatClass = combatClass;
+		this.weapon = weapon;
+	}
 
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public int getCombatClass() {
+		return combatClass;
+	}
+
+	public int getWeapon() {
+		return weapon;
 	}
 
 }
-
-//Vida
-//Ataque
-//Defesa
-
-
-//Nome
-//Sexo
-//Classe de combate
-//Arma disponível
