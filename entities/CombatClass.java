690f3da92afb27282f6697b8f38080ab4ed3c3f9
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2022-01-20 08:58:49 -0300

    Update CombatClass.java

--- entities/CombatClass.java
+++ entities/CombatClass.java
@@ -1,21 +1,14 @@
 package entities;
 
 public abstract class CombatClass {
-	private String combatClass;
 	private int attack;
 	private int defense;
-	private Weapon weapons;
+	private Weapon[] weapons;
 	
-	public CombatClass(String combatClass, int attack, int defense) {
-		super();
-		this.combatClass = combatClass;
+	public CombatClass(int attack, int defense, Weapon[] weapons) {
 		this.attack = attack;
 		this.defense = defense;
 		this.weapons = weapons;
-	}
-
-	public String getCombatClass() {
-		return combatClass;
 	}
 
 	public int getAttack() {
@@ -25,12 +18,9 @@ public abstract class CombatClass {
 	public int getDefense() {
 		return defense;
 	}
-	
-	
+
+	public Weapon[] getWeapons() {
+		return weapons;
+	}
 	
 }
-
-// Guerreiro/Guerreira: espada, machado, martelo
-// Caçador/Caçadora: arco+flecha, besta+virote
-// Sacerdote/Sacerdotisa: clava, livro
-// Feiticeiro/Feiticeira: cajado e livro
